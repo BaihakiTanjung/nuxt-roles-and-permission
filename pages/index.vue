461db63c21eb03a3abc3971490e2734e61a111d4
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2022-11-04 18:41:05 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>index</h1>
+</template>
+<script>
+export default {
+  middleware: 'authenticated',
+  mounted() {
+    console.log(this.$auth)
+    console.log(this.$auth.hasScope('admin'))
+  }
+}
+</script>
